b898de1521fef0472f3008f0eb80796572d35cda
Adithyan66 <adithyanbinu666@gmail.com>
2025-11-23 02:52:14 +0530

    setup completed and login endpoint integrated and tested sucessfully

--- src/presentation/dto/example.dto.ts
+++
@@ -1,4 +0,0 @@
-export interface ExampleDto {
-  name: string;
-  email: string;
-}
--- src/presentation/routes/index.ts
+++ src/presentation/routes/index.ts
@@ -1,9 +1,7 @@
 import { Router } from '../interfaces';
-import { setupExampleRoutes } from './example.routes';
 import { setupAuthRoutes } from './auth/auth.routes';
 
 export const setupRoutes = (router: Router): void => {
-  setupExampleRoutes(router);
   setupAuthRoutes(router);
 };
 
